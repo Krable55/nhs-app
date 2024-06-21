7e6f3917e70353ebd2375c82899311ec2105797b
Kyle Rable <Krable55@gmail.com>
2024-06-21 12:21:46 -0700

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: 'https://cdn.jsdelivr.net/gh/Krable55/nhs-app/dist/',
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './src/main.tsx',
+      },
+    },
+  },
 })
