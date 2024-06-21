8688ed761089262f0cb2a0174af8083cd571c64a
Kyle Rable <Krable55@gmail.com>
2024-06-21 12:45:46 -0700

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
-// <script src="https://cdn.jsdelivr.net/gh/Krable55/nhs-app/dist/assets/app-SfMxGN1-.js"></script>
+// <script src="https://cdn.jsdelivr.net/gh/Krable55/nhs-app/dist/assets/app-Cp1p8702.js"></script>
 
 export default defineConfig({
   base: 'https://cdn.jsdelivr.net/gh/Krable55/nhs-app/dist/',
   plugins: [react(), cssInjectedByJsPlugin()],
   build: {
+    cssCodeSplit: false,
     rollupOptions: {
       input: {
         app: './src/main.tsx',
