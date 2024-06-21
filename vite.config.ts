06f61f16c7757a9899307f097b53b653dcd0eea9
Kyle Rable <Krable55@gmail.com>
2024-06-21 12:31:19 -0700

    update root id

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
+// <script src="https://cdn.jsdelivr.net/gh/Krable55/nhs-app/dist/assets/app-SfMxGN1-.js"></script>
+
 export default defineConfig({
   base: 'https://cdn.jsdelivr.net/gh/Krable55/nhs-app/dist/',
   plugins: [react(), cssInjectedByJsPlugin()],
